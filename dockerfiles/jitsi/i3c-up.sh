15a23f739854516d377a9db33fd373a95dd08ca7
root <root@mblap-5559>
2019-01-03 17:21:08 +0100

    compatibility not works

--- dockerfiles/jitsi/i3c-up.sh
+++ dockerfiles/jitsi/i3c-up.sh
@@ -4,7 +4,7 @@ cd $uData/docker-jitsi-meet
 if [ ! -e $uData/docker-jitsi-meet/.env ]; then
 	cp $uData/docker-jitsi-meet/env.example $uData/docker-jitsi-meet/.env
 fi
-
-docker-compose up -d --compatibility 
+#--compatibility - doesn't work limits ignored - back to dockerfiles...
+docker-compose up -d  
 /i nc docker-jitsi-meet_web_1 bridge
 doCommand=false
