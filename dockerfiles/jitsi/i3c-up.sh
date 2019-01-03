4a3ccb8ebbb30e1ee317bcc526a17a35a25c6314
root <root@mblap-5559>
2019-01-03 17:15:42 +0100

    compatibility mode

--- dockerfiles/jitsi/i3c-up.sh
+++ dockerfiles/jitsi/i3c-up.sh
@@ -5,6 +5,6 @@ if [ ! -e $uData/docker-jitsi-meet/.env ]; then
 	cp $uData/docker-jitsi-meet/env.example $uData/docker-jitsi-meet/.env
 fi
 
-docker-compose up -d 
+docker-compose up -d --compatibility 
 /i nc docker-jitsi-meet_web_1 bridge
 doCommand=false
