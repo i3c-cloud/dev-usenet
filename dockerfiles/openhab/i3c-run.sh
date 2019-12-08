7c577111c597b9ddfab0626c827bd354dbe78a6e
virtimus <virtimus@gmail.com>
2019-12-08 12:26:39 +0100

    openhab user

--- dockerfiles/openhab/i3c-run.sh
+++ dockerfiles/openhab/i3c-run.sh
@@ -1,4 +1,19 @@
 
+id -u openhab
+if [ $? -eq 0 ]; then
+    echo "User openhab exists"
+else
+    echo "User openhab does not exist - creating ..."
+    sudo useradd -r -s /sbin/nologin openhab
+fi
+
+    usermod -a -G openhab $USER
+    mkdir -p $uData
+	mkdir -p $uData/conf
+	mkdir -p $uData/userdata
+	mkdir -p $uData/addons
+	chown -R openhab:openhab $uData
+
 dParams="-d \
         --name openhab \
         --net=host \
